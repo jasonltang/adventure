c0dc3880a5262ad2d38e1b89fc64f4343cb8bc5a
Jason <jasonltang@gmail.com>
2019-02-10 21:51:54 +1100

    Fix notes

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -1,7 +1,4 @@
 ﻿// Todo list
-// Separate monsters by stage
-// Give every enemy an attack
-// Add bosses for each level
 // Add some more difficult monsters
 // Introduce 'standard deviation' of attacks
 // Set a ceiling for leveling up, i.e. don't throw error when beating lv 10
